8337c9de19756821b9819491b9042e6eb64dec52
Johnny <johnnyyedward@gmail.com>
2024-05-19 20:49:38 -0700

    added connector

--- README.md
+++ README.md
@@ -1,15 +1,13 @@
 <p align="center">
   <img src="https://github.com/jonatng/genai/blob/main/images/artifical_intelligenc.jpeg" width="400"/>
 </p>
-----
 
-# Genai
+# GEN-AI
 Perform data analysis and visualization with LLM
-```
-Pandas AI 
-GROQ
-Mixtral 8x7b
-```
+> Pandas AI 
+> GROQ
+> Mixtral 8x7b
+
 ### Create GROQ(Graph-Relational Object Queries) API Key
 ```
 https://console.groq.com/playground 
@@ -19,8 +17,17 @@ Then add the GROQ API Key to .env
 ```
 pip3 install -r requirements.txt 
 ```
+### Install MySQL (Optional to use MySQL)
+> https://dev.mysql.com/downloads/mysql/
+> https://www.youtube.com/watch?v=ODA3rWfmzg8 
+
+### Create MySQL Datebases (Optional to use MySQL)
+
 
 ### Initialize the model via terminal
 ```
 streamlit run app.py
 ```
+
+### TO DO
+# Docker Container
--- app.py
+++ app.py
@@ -3,7 +3,20 @@ from dotenv import load_dotenv
 from langchain_groq.chat_models import ChatGroq
 import pandas as pd 
 from pandasai import SmartDataframe
+from pandasai.connectors import MySQLConnector
 import os
+
+my_connector = MySQLConnector(
+    config={
+        'host': 'localhost',
+        'port': 3306,
+        'database': 'genai',
+        'username': 'root',
+        'password': 'abcd1234',
+        'table': 'genai.heart_disease',
+        'table': 'genai.thyroid_disease'
+    }
+)
 
 load_dotenv(override=True)
 
@@ -11,7 +24,18 @@ model = ChatGroq(
     model ="mixtral-8x7b-32768", 
     api_key = os.environ["GROQ_API_KEY"])
 
-st.title("Data Analysis with PandasAI")
+df_connector = SmartDataframe(my_connector, config={"llm": model})
+
+st.title("Data Analysis with MySQL")
+
+prompt = st.text_input("Enter your prompt:")
+
+if st.button("Generate"):
+    if prompt:
+        with st.spinner("Generating response..."):
+            st.write(df_connector.chat(prompt))
+
+st.title("Data Analysis with CSV")
 
 uploaded_file = st.file_uploader("Upload a CSV file", type=['csv'])
 
